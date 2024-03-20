111e7e02c28d67087fce0b0fac892013ec6a8abf
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2024-03-20 10:08:59 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,234 @@
+list_of_movies = [
+    {
+        "title": "The Shawshank Redemption",
+        "year": 1994,
+        "director": "Frank Darabont",
+        "genre": ["Drama", "Crime"],
+        "imdb_rating": 9.3,
+        "runtime": 142,
+        "actors": ["Tim Robbins", "Morgan Freeman"],
+        "country": "USA",
+        "awards": [
+            {
+                "name": "Academy Award",
+                "category": "Best Picture"
+            },
+            {
+                "name": "Golden Globe Award",
+                "category": "Best Drama Film"
+            }
+        ]
+    },
+    {
+        "title": "The Godfather",
+        "year": 1972,
+        "director": "Francis Ford Coppola",
+        "genre": ["Crime", "Drama"],
+        "imdb_rating": 9.2,
+        "actors": [
+            "Marlon Brando",
+            "Al Pacino",
+            "James Caan",
+        ],
+        "runtime": 177,
+        "country": "USA",
+        "awards": [{
+            "name": "Academy Award",
+            "category": "Best Picture"
+        },
+            {
+                "name": "Golden Globe Award",
+                "category": "Best Drama Film"
+            }]
+    },
+    {
+        "title": "The Dark Knight",
+        "year": 2008,
+        "director": "Christopher Nolan",
+        "genre": ["Action", "Thriller"],
+        "imdb_rating": 9.0,
+        "actors": [
+            "Christian Bale",
+            "Heath Ledger",
+            "Aaron Eckhart"
+        ],
+        "runtime": 152,
+        "country": "USA",
+        "awards": [{
+            "name": "Academy Award",
+            "category": "Best Sound Editing"
+        },
+            {
+                "name": "Golden Globe Award",
+                "category": "Best Supporting Actor - Motion Picture"
+            }]
+    },
+    {
+        "title": "The Lord of the Rings: The Return of the King",
+        "year": 2003,
+        "director": "Peter Jackson",
+        "genre": ["Fantasy", "Adventure"],
+        "imdb_rating": 8.9,
+        "actors": [
+            "Elijah Wood",
+            "Viggo Mortensen",
+            "Ian McKellen",
+            "Orlando Bloom",
+            "Sean Astin",
+            "Cate Blanchett",
+        ],
+        "runtime": 201,
+        "country": "New Zealand",
+        "awards": [
+            "Academy Award - Best Picture",
+            "Academy Award - Best Director (Peter Jackson)",
+            "Academy Award - Best Adapted Screenplay",
+            "Academy Award - Best Original Score",
+            "Academy Award - Best Visual Effects",
+            "Golden Globe Award - Best Motion Picture - Drama",
+        ]
+    },
+    {
+        "title": "Pulp Fiction",
+        "year": 1994,
+        "director": "Quentin Tarantino",
+        "genre": ["Crime", "Comedy"],
+        "imdb_rating": 8.9,
+        "actors": [
+            "John Travolta",
+            "Samuel L. Jackson",
+            "Uma Thurman",
+            "Tim Roth",
+            "Amanda Plummer",
+            "Ving Rhames",
+        ],
+        "runtime": 154,
+        "country": "USA",
+        "awards": [
+            "Academy Award - Best Original Screenplay",
+            " Palme d'Or at Cannes Film Festival",
+
+        ]
+    },
+    {
+        "title": "Schindler's List",
+        "year": 1993,
+        "director": "Steven Spielberg",
+        "genre": ["Historical", "Drama"],
+        "imdb_rating": 8.9,
+        "actors": [
+            "Liam Neeson",
+            "Ralph Fiennes",
+            "Ben Kingsley",
+            "Olivia Dahan",
+            "Fionnula Flanagan",
+        ],
+        "runtime": 195,
+        "country": "USA",
+        "awards": [
+            "Academy Award - Best Picture",
+            "Academy Award - Best Director (Steven Spielberg)",
+            "Academy Award - Best Adapted Screenplay",
+            "Academy Award - Best Cinematography",
+            "Academy Award - Best Original Score",
+            "Golden Globe Award - Best Motion Picture - Drama",
+            "Golden Globe Award - Best Director (Steven Spielberg)",
+        ]
+    },
+    {
+        "title": "12 Angry Men",
+        "year": 1957,
+        "director": "Sidney Lumet",
+        "genre": ["Drama", "Crime"],
+        "imdb_rating": 8.9,
+        "actors": [
+            "Henry Fonda",
+            "Lee J. Cobb",
+            "Martin Balsam",
+            "E.G. Marshall",
+            "Jack Warden",
+            "Ed Binns",
+            "John Fiedler",
+        ],
+        "runtime": 96,
+        "country": "USA",
+        "awards": [
+            "Golden Globe Award - Best Motion Picture - Drama"
+        ]
+    },
+    {
+        "title": "The Lord of the Rings: The Fellowship of the Ring",
+        "year": 2001,
+        "director": "Peter Jackson",
+        "genre": ["Fantasy", "Adventure"],
+        "imdb_rating": 8.8,
+        "actors": [
+            "Elijah Wood",
+            "Ian McKellen",
+            "Orlando Bloom",
+            "Viggo Mortensen",
+            "Sean Astin",
+            "Cate Blanchett",
+            "Liv Tyler",
+            "Sean Bean",
+            "John Rhys-Davies",
+            "Billy Boyd",
+            "Dominic Monaghan",
+
+        ],
+        "runtime": 208,
+        "country": "New Zealand",
+        "awards": [
+            "Academy Award - Best Adapted Screenplay",
+            "Academy Award - Best Cinematography",
+            "Academy Award - Best Makeup",
+            "Academy Award - Best Original Score",
+            "Golden Globe Award - Best Original Score",
+
+        ]
+    },
+    {
+        "title": "Fight Club",
+        "year": 1999,
+        "director": "David Fincher",
+        "genre": ["Drama", "Thriller"],
+        "imdb_rating": 8.8,
+        "actors": [
+            "Brad Pitt",
+            "Edward Norton",
+            "Helena Bonham Carter",
+            "Meat Loaf",
+            "Jared Leto",
+
+        ],
+        "runtime": 139,
+        "country": "USA",
+        "awards": []
+    },
+    {
+        "title": "Forrest Gump",
+        "year": 1994,
+        "director": "Robert Zemeckis",
+        "genre": ["Drama", "Romance"],
+        "imdb_rating": 8.8,
+        "actors": [
+            "Tom Hanks",
+            "Robin Wright",
+            "Gary Sinise",
+            "Mykelti Williamson",
+            "Sally Field",
+        ],
+        "runtime": 142,
+        "country": "USA",
+        "awards": [
+            "Academy Award - Best Picture",
+            "Academy Award - Best Actor (Tom Hanks)",
+            "Academy Award - Best Director (Robert Zemeckis)",
+            "Academy Award - Best Adapted Screenplay",
+            "Academy Award - Best Film Editing",
+            "Golden Globe Award - Best Motion Picture - Drama",
+            "Golden Globe Award - Best Actor - Motion Picture Drama (Tom Hanks)",
+
+        ]
+    }
+]
