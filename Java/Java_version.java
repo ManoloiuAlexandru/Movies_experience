e665848b1247ebd1e215cf9a9e44d09642316b21
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2024-03-21 14:35:09 +0200

    Update Java_version.java

--- Java/Java_version.java
+++ Java/Java_version.java
@@ -1,5 +1,7 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -104,6 +106,49 @@ public class Main {
         return countryMovies;
     }
 
+    public static String generateRandomSalt() {
+        return RandomStringUtils.randomAlphanumeric(8);
+    }
+
+    public static void saltThePasswords(ArrayList<Map<String, String>> listOfUsers) {
+        for (Map<String, String> user : listOfUsers) {
+            user.put("salt", generateRandomSalt());
+            user.put("hash", user.get("password") + String.valueOf(user.get("salt")).hashCode());
+        }
+    }
+
+    public static void encryptEmail(ArrayList<Map<String, String>> listOfUsers) {
+        int shifting = 5;
+        for (Map<String, String> user : listOfUsers) {
+            String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            String email = user.get("email").split("@")[0];
+            StringBuilder encryptedEmail = new StringBuilder();
+            for (int i = 0; i < email.length(); i++) {
+                if (alphabet.contains(String.valueOf(email.charAt(i))))
+                    encryptedEmail.append(alphabet.charAt((alphabet.indexOf(email.charAt(i)) + shifting) % 52));
+
+                else
+                    encryptedEmail.append(email.charAt(i));
+
+            }
+            System.out.println(encryptedEmail);
+        }
+    }
+
+    public static boolean passwordCheck(String password) {
+        boolean upper = false, lower = false, number = false;
+        for (int i = 0; i < password.length(); i++) {
+            if (StringUtils.isAllUpperCase(String.valueOf(password.charAt(i)))) {
+                upper = true;
+            } else if (StringUtils.isAllLowerCase(String.valueOf(password.charAt(i)))) {
+                lower = true;
+            } else if (StringUtils.isNumeric(String.valueOf(password.charAt(i)))) {
+                number = true;
+            }
+        }
+        return upper && lower && number && password.length() >= 8;
+    }
+
     public static void main(String[] args) {
         Map<Object, Object> responseFromServer;
         try {
@@ -133,6 +178,43 @@ public class Main {
             System.out.println(getBestMakeupMovies(movies));
             System.out.println(getMapOfMovieCountry(movies));
         }
+        ArrayList<Map<String, String>> listOfUsers = getMaps();
+        saltThePasswords(listOfUsers);
+        Scanner myObj = new Scanner(System.in);
+        String userName = myObj.nextLine();
+        String enteredPassword = myObj.nextLine();
+        for (Map<String, String> user : listOfUsers) {
+            if (user.get("nume").equals(userName)) {
+                if ((enteredPassword + user.get("salt").hashCode()).equals(user.get("hash"))) {
+                    System.out.println("Correct Password");
+                }
+            }
+        }
+        System.out.println(passwordCheck(listOfUsers.get(1).get("password")));
+        encryptEmail(listOfUsers);
+    }
+
+    private static ArrayList<Map<String, String>> getMaps() {
+        ArrayList<Map<String, String>> listOfUsers = new ArrayList<>();
+        Map<String, String> entry = new HashMap<>() {{
+            put("nume", "Ion");
+            put("password", "df3213d");
+            put("email", "AAASS@yahoo.com");
+        }};
+        listOfUsers.add(entry);
+        entry = new HashMap<>() {{
+            put("nume", "Vasile");
+            put("email", "sfsgsddsfg@yahoo.com");
+            put("password", "Ad5kfkgfJHH90hjh8y");
+        }};
+        listOfUsers.add(entry);
+        entry = new HashMap<>() {{
+            put("nume", "Mircea");
+            put("email", "dsfsdgfdf@yahoo.com");
+            put("password", "df322fd3n32K13d");
+        }};
+        listOfUsers.add(entry);
+        return listOfUsers;
     }
 
     private static Map<Object, Object> getJsonObject(URL url) throws IOException {
