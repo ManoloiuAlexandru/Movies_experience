556a259c19f192018edc659c9599403d2a1215ac
lightyng <157044710+lightyng@users.noreply.github.com>
2024-03-26 23:44:28 +0200

    Add files via upload

---
+++ data.py
@@ -0,0 +1,95 @@
+new_movie = {
+    "title": "The 400 Blows",
+    "year": 1959,
+    "director": "François Truffaut",
+    "genre": ["Drama", "Crime"],
+    "imdb_rating": 8.1,
+    "runtime": 139,
+    "actors": ["Jean-Pierre Léaud", "Claire Maurier"],
+    "country": "France",
+    "awards": [
+        {
+            "name": "Academy Award",
+            "category": "Best writting,story and screenplay"
+        },
+        {
+
+        }
+    ]
+}
+
+catalin_movies = [
+    {
+        "title": "Gone Girl",
+        "year": 2014,
+        "director": "David Fincher",
+        "genre": ["Mystery", "Thriller"],
+        "imdb_rating": 8.1,
+        "actors": ["Ben Affleck", "Rosamund Pike", "Neil Patrick Harris"],
+        "runtime": 149,
+        "country": "USA",
+        "awards": [
+            "Academy Awards - Best Actress in a Leading Role"
+        ]
+    },
+    {
+        "title": "Inception",
+        "year": 2010,
+        "director": "Christopher Nolan",
+        "genre": ["Science Fiction", "Action"],
+        "imdb_rating": 8.8,
+        "actors": ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"],
+        "runtime": 148,
+        "country": "USA",
+        "awards": [
+            "Academy Awards - Best Cinematography"
+        ]
+    },
+    {
+        "title": "The Matrix",
+        "year": 1999,
+        "director": "The Wachowskis",
+        "genre": ["Action", "Sci-Fi"],
+        "imdb_rating": 8.7,
+        "actors": ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
+        "runtime": 136,
+        "country": "USA",
+        "awards": [
+            "Academy Award - Best Adapted Screenplay",
+            "Academy Award - Best Cinematography",
+            "Academy Award - Best Makeup",
+            "Academy Award - Best Original Score",
+            "Golden Globe Award - Best Original Score"
+        ]
+    },
+    {
+        "title": "Terminator 2: Judgment Day",
+        "year": 1991,
+        "director": "James Cameron",
+        "genre": ["Action", "Sci-Fi"],
+        "imdb_rating": 8.5,
+        "actors": ["Arnold Schwarzenegger", "Linda Hamilton", "Edward Furlong"],
+        "runtime": 137,
+        "country": "USA",
+        "awards": [
+            "Academy Award - Best Sound",
+            "Academy Award - Best Makeup",
+            "Academy Award - Best Visual Effects"
+        ]
+    },
+    {
+        "title": "Se7en",
+        "year": 1995,
+        "director": "David Fincher",
+        "genre": ["Crime", "Drama", "Mystery"],
+        "imdb_rating": 8.6,
+        "actors": ["Morgan Freeman", "Brad Pitt", "Kevin Spacey"],
+        "runtime": 127,
+        "country": "USA",
+        "awards": [
+            "MTV Movie Award - Best Movie"
+            "MTV Movie Award - Best Breakthrough Performance"
+        ]
+    }
+]
+ceva = "ceva"
